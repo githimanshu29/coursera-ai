d572841199e940396f0c28907f3a3f5ecdafdb37
Anupthakurr <tanup229@gmail.com>
2026-09-13 00:46:25 +0530

    Fix import paths in Billing.jsx that caused build failure

--- client/src/pages/workspace/Billing.jsx
+++ client/src/pages/workspace/Billing.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getMeApi } from "../../../lib/api.js";
-import { updateCredits } from "../../../store/slices/authSlice.js";
+import { getMeApi } from "../../lib/api.js";
+import { updateCredits } from "../../store/slices/authSlice.js";
 
 const Billing = () => {
   const { user } = useSelector((state) => state.auth);
